39af45332fe4565502e7e96c220ef1d1941cdda1
Artur Malyshev <42480216+ArturMalyshev@users.noreply.github.com>
2022-03-15 16:51:05 +0300

    Гость. Добавил адаптивности

---
+++ script/display.js
@@ -0,0 +1,13 @@
+let width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+const phoneDisplayWidth = 515;
+
+const addMenuButton = () => {
+    const menuButton = document.createElement('button');
+    menuButton.classList.add('menu-button');
+};
+
+if (width <= phoneDisplayWidth){
+
+}
